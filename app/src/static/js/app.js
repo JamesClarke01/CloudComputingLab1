f845565c3ec3d59b249db0da3ce8693b1974fdce
Ubuntu <c20375736@james.gw3530h4snkuni02tezybgqg3g.phxx.internal.cloudapp.net>
2022-10-19 11:26:52 +0000

    Work from lab 2, includes creative changes

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -21,8 +21,13 @@ function TodoListCard() {
     }, []);
 
     const onNewItem = React.useCallback(
-        newItem => {
-            setItems([...items, newItem]);
+	newItem => {
+	if(items.length < 5){
+		setItems([...items, newItem]);
+	}
+	else{
+		alert("Cannot add more than 5 items to the list!");
+	}
         },
         [items],
     );
@@ -53,7 +58,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+		<p className="text-center">You have no todo items yet! Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
@@ -106,7 +111,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
